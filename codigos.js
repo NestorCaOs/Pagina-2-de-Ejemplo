00e82e074f09485888ecddfd04a6db68c323038f
Nesthor Heads <nestorcabezas@gmail.com>
2022-06-13 04:54:49 -0400

    Update codigos.js

--- codigos.js
+++ codigos.js
@@ -1,10 +1,7 @@
 const contenedor = document.querySelector(".flex-container");
 
-const boton = document.querySelector(".send-button");
-boton.value = boton.value.toUpperCase();
 
-function crearLlave(nombre,modelo,precio){
-	img = "<img class='llave-img' src='https://thumbs.dreamstime.com/z/llaves-antiguas-27891623.jpg'>";//siempre png
+function crearLlave(nombre,modelo,precio,img){
 	nombre = `<h2>${nombre}</h2>`;
 	//modelo = `<h3 id='${modelo}'>${modelo}</h3>`;
 	modelo = `<h3>${modelo}</h3>`;
@@ -18,10 +15,23 @@ const changeHidden = (number)=>{
 
 let documentFragment = document.createDocumentFragment();
 
-for (var i = 1; i <= 20; i++){
+let imgg=""
+for (var i = 1; i <= 4; i++){
+	if (i == 1) {
+			imgg = "<img class='llave-img' src='https://www.revistafamilia.ec/files/article_main/uploads/2021/08/04/610afc507d1c1.jpeg'>";
+	} else if (i == 2) {
+			imgg = "<img class='llave-img' src='https://img-global.cpcdn.com/recipes/98cbf269a99506bb/680x482cq70/almuerzo-de-domingo-carne-foto-principal.webp'>";
+	} else if (i == 3) {
+			imgg = "<img class='llave-img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAqSzTe_3SzlssKeFPJNosjqAhABqW6oi-6V59e1BzbCi-qmhFr3tysbJoSF2PRG51UyY&usqp=CAU'>";
+	} else if (i == 4) {
+			imgg = "<img class='llave-img' src='https://i.blogs.es/b8b596/apertura-apaisada-ensaladas/1024_2000.jpg'>";
+	};
+
+
+
 	let modeloRandom = Math.round(Math.random()*10000);
 	let precioRandom = Math.round(Math.random()*10+30);
-	let llave = crearLlave(`Llave ${i}`,`Modelo ${modeloRandom}`, precioRandom);
+	let llave = crearLlave(`Llave ${i}`,`Modelo ${modeloRandom}`, precioRandom,imgg);
 	let div = document.createElement("DIV");
 	// div.addEventListener("click",()=>{changeHidden(modeloRandom)});
 	div.addEventListener("click",()=>{
